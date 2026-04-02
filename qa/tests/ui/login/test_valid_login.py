95d41f94b0fa00a430fdc26922587da5f33e88aa
Chris <c_melski@yahoo.com>
2026-04-02 19:41:16 +0700

    updated files

--- qa/tests/ui/login/test_valid_login.py
+++ qa/tests/ui/login/test_valid_login.py
@@ -1,3 +1,4 @@
+import os
 
 from playwright.sync_api import Page, expect
 from qa.pages.login import LoginPage
@@ -9,7 +10,7 @@ import pytest
 @pytest.mark.valid_login
 def test_valid_login(page_instance_login_tests: Page) -> None:
     login_page = LoginPage(page_instance_login_tests)
-    login_page.login('c_melski@yahoo.com', 'bestDAY2011$')
+    login_page.login(os.environ.get('USER_EMAIL'), os.environ.get('USER_PASSWORD'))
     dashboard_page = DashboardPage(page_instance_login_tests)
     expect(dashboard_page.page_title).to_be_visible(timeout=3000)
     expect(dashboard_page.page_title).to_have_text('Dashboard')
